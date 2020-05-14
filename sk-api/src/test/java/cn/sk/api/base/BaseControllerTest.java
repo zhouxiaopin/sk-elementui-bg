f7f2870b69e3c067deab18a8d69f47b8b0eed89e
zhoucp <1156693737@qq.com>
2020-05-14 11:06:17 +0800

    添加了父类

---
+++ sk-api/src/test/java/cn/sk/api/base/BaseControllerTest.java
@@ -0,0 +1,34 @@
+package cn.sk.api.base;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WebAppConfiguration
+public class BaseControllerTest {
+    protected MockMvc mockMvc; // 模拟MVC对象，通过MockMvcBuilders.webAppContextSetup(this.wac).build()初始化。
+
+    @Autowired
+    protected WebApplicationContext wac; // 注入WebApplicationContext
+
+//    @Autowired
+//    private MockHttpSession session;// 注入模拟的http session
+//
+//    @Autowired
+//    private MockHttpServletRequest request;// 注入模拟的http request\
+
+    @Before // 在测试开始前初始化工作
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+}
---
+++ sk-api/src/test/java/cn/sk/api/sys/service/impl/SysUserServiceImplTest.java
@@ -0,0 +1,34 @@
+package cn.sk.api.sys.service.impl;
+
+import cn.sk.api.base.BaseServiceTest;
+import cn.sk.api.sys.pojo.SysUser;
+import cn.sk.api.sys.service.ISysUserService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @Deseription
+ * @Author zhoucp
+ * @Date 2020/5/14 10:52
+ */
+public class SysUserServiceImplTest extends BaseServiceTest {
+    @Autowired
+    ISysUserService sysUserService;
+
+    @Test
+    public  void insert() {
+        SysUser sysUser = new SysUser();
+        sysUser.setUserName("用户名test");
+        sysUser.setPassword("123456");
+        sysUser.setEmail("1156693435");
+        sysUserService.insert(sysUser);
+    }
+    @Test
+    public  void update() {
+        SysUser sysUser = new SysUser();
+        sysUser.setUserId(36);
+        sysUser.setEmail("1156");
+        sysUserService.update(sysUser);
+    }
+
+}
